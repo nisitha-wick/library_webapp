575b778172905e0ddc654bf1f4d1124ab6b30a6b
Nisitha <wick.nisitha@gmail.com>
2024-07-18 21:02:29 +0530

    Upgrading the UI

--- resources/js/components/books/Index.vue
+++ resources/js/components/books/Index.vue
@@ -4,41 +4,45 @@
         <h2>Where all your books needs are met</h2>
     </div>
 
+    <div class="InputContainer">
+        <input placeholder="Search by Title" id="input" class="input" name="text">
+    </div>
+
     <div class="product-grid">
         <div class="product-item">
             <img src="C:\Users\wickn\Desktop\Install-Laravel\library\laravel\public\book imgs\lotr.jpg">
-            <h3>Lord of the Rings</h3>
+            <h3>The Lord of the Rings</h3>
             <p>Rs.5000.00</p>
         </div>
 
         <div class="product-item">
             <img src="C:\Users\wickn\Desktop\Install-Laravel\library\laravel\public\book imgs\chamber of secrets.jpg">
-            <h3>Harry Potter</h3>
-            <p>Rs.5000.00</p>
+            <h3>Harry Potter and The Chamber of Secrets</h3>
+            <p>Rs.4000.00</p>
         </div>
 
         <div class="product-item">
             <img src="C:\Users\wickn\Desktop\Install-Laravel\library\laravel\public\book imgs\ice&fire.jpg">
-            <h3>Harry Potter</h3>
-            <p>Rs.5000.00</p>
+            <h3>The World of Ice & Fire</h3>
+            <p>Rs.6000.00</p>
         </div>
 
         <div class="product-item">
             <img src="C:\Users\wickn\Desktop\Install-Laravel\library\laravel\public\book imgs\witcher.jpg">
-            <h3>Harry Potter</h3>
-            <p>Rs.5000.00</p>
+            <h3>The Witcher</h3>
+            <p>Rs.2500.00</p>
         </div>
 
         <div class="product-item">
             <img src="C:\Users\wickn\Desktop\Install-Laravel\library\laravel\public\book imgs\spiderman.jpg">
-            <h3>Harry Potter</h3>
-            <p>Rs.5000.00</p>
+            <h3>Amazing Book of Spider Man</h3>
+            <p>Rs.2000.00</p>
         </div>
 
         <div class="product-item">
             <img src="C:\Users\wickn\Desktop\Install-Laravel\library\laravel\public\book imgs\witcher.jpg">
-            <h3>Harry Potter</h3>
-            <p>Rs.5000.00</p>
+            <h3>The Witcher</h3>
+            <p>Rs.2000.00</p>
         </div>
     </div>
 </template>
