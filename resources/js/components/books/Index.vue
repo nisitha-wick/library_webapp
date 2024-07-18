b9a3457a6a07ac7416c72a22ec8d92311d9e784c
Nisitha <wick.nisitha@gmail.com>
2024-07-18 22:03:32 +0530

    Update styling and layout for the Online Library page

--- resources/js/components/books/Index.vue
+++ resources/js/components/books/Index.vue
@@ -1,7 +1,7 @@
 <template>
-    <div>
+    <div class="header">
         <h1>Online Library</h1>
-        <h2>Where all your books needs are met</h2>
+        <h2>Where all your books needs are met.</h2>
     </div>
 
     <div class="InputContainer">
@@ -10,39 +10,45 @@
 
     <div class="product-grid">
         <div class="product-item">
-            <img src="C:\Users\wickn\Desktop\Install-Laravel\library\laravel\public\book imgs\lotr.jpg">
+            <img src="\public\book imgs\lotr.jpg">
             <h3>The Lord of the Rings</h3>
             <p>Rs.5000.00</p>
+            <button>Borrow</button>
         </div>
 
         <div class="product-item">
-            <img src="C:\Users\wickn\Desktop\Install-Laravel\library\laravel\public\book imgs\chamber of secrets.jpg">
+            <img src="\public\book imgs\chamber of secrets.jpg">
             <h3>Harry Potter and The Chamber of Secrets</h3>
             <p>Rs.4000.00</p>
+            <button>Borrow</button>
         </div>
 
         <div class="product-item">
-            <img src="C:\Users\wickn\Desktop\Install-Laravel\library\laravel\public\book imgs\ice&fire.jpg">
+            <img src="\public\book imgs\ice&fire.jpg">
             <h3>The World of Ice & Fire</h3>
             <p>Rs.6000.00</p>
+            <button>Borrow</button>
         </div>
 
         <div class="product-item">
-            <img src="C:\Users\wickn\Desktop\Install-Laravel\library\laravel\public\book imgs\witcher.jpg">
+            <img src="\public\book imgs\witcher.jpg">
             <h3>The Witcher</h3>
             <p>Rs.2500.00</p>
+            <button>Borrow</button>
         </div>
 
         <div class="product-item">
-            <img src="C:\Users\wickn\Desktop\Install-Laravel\library\laravel\public\book imgs\spiderman.jpg">
+            <img src="\public\book imgs\spiderman.jpg">
             <h3>Amazing Book of Spider Man</h3>
             <p>Rs.2000.00</p>
+            <button>Borrow</button>
         </div>
 
         <div class="product-item">
-            <img src="C:\Users\wickn\Desktop\Install-Laravel\library\laravel\public\book imgs\witcher.jpg">
+            <img src="\public\book imgs\witcher.jpg">
             <h3>The Witcher</h3>
             <p>Rs.2000.00</p>
+            <button>Borrow</button>
         </div>
     </div>
 </template>
