22a9236607faa654085f7f37586887f8cf49dc47
Nisitha <wick.nisitha@gmail.com>
2024-07-18 19:55:26 +0530

    setting up the project

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 import './bootstrap';
+
+import { createApp } from 'vue';
+
+import app from './components/App.vue';
+
+import router from './router';
+
+createApp(app).use(router).mount("#app")
---
+++ resources/js/router/index.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import bookIndex from '../components/books/Index.vue'
+
+const routes = [{
+    path:'/',
+    name: 'books.index',
+    component: bookIndex
+}]
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+export default router;
