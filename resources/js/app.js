4123279fb62be51c257f84d36cb5aee1eeecff48
Nisitha <wick.nisitha@gmail.com>
2024-07-20 17:03:53 +0530

    breeze

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,23 @@
 import './bootstrap';
+import '../css/app.css';
 
-import { createApp } from 'vue';
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 
-import app from './components/App.vue';
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
-import router from './router';
-
-createApp(app).use(router).mount("#app")
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    setup({ el, App, props, plugin }) {
+        return createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue)
+            .mount(el);
+    },
+    progress: {
+        color: '#4B5563',
+    },
+});
