ea9e7e26f8e13dbcaaeeb6ccf33886984d07ab33
Nisitha <wick.nisitha@gmail.com>
2024-07-22 18:51:28 +0530

    completed registration and login

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\BookController;
 use App\Http\Controllers\HomeController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('register');
 });
 
 Auth::routes();
